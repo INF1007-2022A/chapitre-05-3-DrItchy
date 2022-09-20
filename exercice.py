47a1d7f64829e60ab237010fd70884b72124066a
DrItchy <thomas.simard@polymtl.ca>
2022-09-20 18:49:45 -0400

    FINI

--- exercice.py
+++ exercice.py
@@ -1,24 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import math
 
 def get_num_letters(text):
-	return 0
+    c=0
+    for i in range(len(text)):
+       if text[i].isalnum():
+           c+=1
+    return c
 
 def get_word_length_histogram(text):
-	return [0]
+    l,c=[],0
+    for i in range(len(text)):
+        if text[i].isalnum() or text[i].isspace():
+            c+=1
+            if text[i].isspace():
+                c-=1
+                while len(l)-1<c:
+                    l.append(0)
+                l[c]+=1
+                c=0
+    while len(l)-1<c:
+        l.append(0)
+    l[c]+=1
+    return l
 
 def format_histogram(histogram):
-	ROW_CHAR = "*"
-
-	return ""
+    ROW_CHAR = "*"
+    for i in range(len(histogram)):
+        print(f"{i} {histogram[i]*ROW_CHAR}")
 
 def format_horizontal_histogram(histogram):
-	BLOCK_CHAR = "|"
-	LINE_CHAR = "¯"
-
-	return ""
+    BLOCK_CHAR = "|"
+    LINE_CHAR = "¯"
+    m,l=max(histogram),""
+    while m!=0:
+        for i in range(len(histogram)):
+            if histogram[i]>=m:
+                l+=BLOCK_CHAR
+            else:
+                l+=" "
+        print(l)
+        l=""
+        m-=1
+    print(LINE_CHAR*len(histogram))
 
 
 if __name__ == "__main__":
